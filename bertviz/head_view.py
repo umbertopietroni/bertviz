d3676c88622a10f1d4aa5d2c905056820a4aea60
Umberto Pietroni <u.pietroni@gmail.com>
2020-09-22 11:12:31 +0200

    Modify head_view.py to support generated sentence and input sentence

--- bertviz/head_view.py
+++ bertviz/head_view.py
@@ -4,7 +4,7 @@ import os
 from .util import format_special_chars, format_attention
 
 
-def head_view(attention, tokens, sentence_b_start = None, prettify_tokens=True):
+def head_view(attention, gen_tokens, input_tokens, sentence_b_start=None, prettify_tokens=True):
     """Render head view
 
         Args:
@@ -38,51 +38,52 @@ def head_view(attention, tokens, sentence_b_start = None, prettify_tokens=True):
             """
 
     display(HTML(vis_html))
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    vis_js = open(os.path.join(__location__, 'head_view.js')).read()
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    vis_js = open(os.path.join(__location__, "head_view.js")).read()
 
     if prettify_tokens:
-        tokens = format_special_chars(tokens)
+        gen_tokens = format_special_chars(gen_tokens)
+        input_tokens = format_special_chars(input_tokens)
 
     attn = format_attention(attention)
     attn_data = {
-        'all': {
-            'attn': attn.tolist(),
-            'left_text': tokens,
-            'right_text': tokens
-        }
+        "all": {"attn": attn.tolist(), "left_text": gen_tokens, "right_text": input_tokens}
     }
     if sentence_b_start is not None:
         slice_a = slice(0, sentence_b_start)  # Positions corresponding to sentence A in input
-        slice_b = slice(sentence_b_start, len(tokens))  # Position corresponding to sentence B in input
-        attn_data['aa'] = {
-            'attn': attn[:, :, slice_a, slice_a].tolist(),
-            'left_text': tokens[slice_a],
-            'right_text': tokens[slice_a]
+        slice_b = slice(
+            sentence_b_start, len(gen_tokens)
+        )  # Position corresponding to sentence B in input
+        attn_data["aa"] = {
+            "attn": attn[:, :, slice_a, slice_a].tolist(),
+            "left_text": gen_tokens[slice_a],
+            "right_text": input_tokens[slice_a],
         }
-        attn_data['bb'] = {
-            'attn': attn[:, :, slice_b, slice_b].tolist(),
-            'left_text': tokens[slice_b],
-            'right_text': tokens[slice_b]
+        attn_data["bb"] = {
+            "attn": attn[:, :, slice_b, slice_b].tolist(),
+            "left_text": gen_tokens[slice_b],
+            "right_text": input_tokens[slice_b],
         }
-        attn_data['ab'] = {
-            'attn': attn[:, :, slice_a, slice_b].tolist(),
-            'left_text': tokens[slice_a],
-            'right_text': tokens[slice_b]
+        attn_data["ab"] = {
+            "attn": attn[:, :, slice_a, slice_b].tolist(),
+            "left_text": gen_tokens[slice_a],
+            "right_text": input_tokens[slice_b],
         }
-        attn_data['ba'] = {
-            'attn': attn[:, :, slice_b, slice_a].tolist(),
-            'left_text': tokens[slice_b],
-            'right_text': tokens[slice_a]
+        attn_data["ba"] = {
+            "attn": attn[:, :, slice_b, slice_a].tolist(),
+            "left_text": gen_tokens[slice_b],
+            "right_text": input_tokens[slice_a],
         }
-    params = {
-        'attention': attn_data,
-        'default_filter': "all"
-    }
-    attn_seq_len = len(attn_data['all']['attn'][0][0])
-    if attn_seq_len != len(tokens):
-        raise ValueError(f"Attention has {attn_seq_len} positions, while number of tokens is {len(tokens)}")
+    params = {"attention": attn_data, "default_filter": "all"}
+    attn_seq_len = len(attn_data["all"]["attn"][0][0])
+    if len(gen_tokens) != len(input_tokens):
+        raise ValueError(
+            f"Gen_tokens has {len(gen_tokens)} positions, while number of input_tokens is {len(input_tokens)}"
+        )
+    if attn_seq_len != len(gen_tokens):
+        raise ValueError(
+            f"Attention has {attn_seq_len} positions, while number of tokens is {len(gen_tokens)}"
+        )
 
-    display(Javascript('window.params = %s' % json.dumps(params)))
+    display(Javascript("window.params = %s" % json.dumps(params)))
     display(Javascript(vis_js))
